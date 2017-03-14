86cf4ba89e91568ed58a90351da182b33ca8bb7b
Linguistics Student's GitHub <student.linguistics@gmail.com>
2017-03-13 23:02:30 -0400

    Update search_trends_api.py

--- search_trends_api.py
+++ search_trends_api.py
@@ -1,29 +1,29 @@
+
+from credentials import *
+
 # Import the necessary methods from "twitter" library
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = 'YOUR ACCESS TOKEN"'
-ACCESS_SECRET = 'YOUR ACCESS TOKEN SECRET'
-CONSUMER_KEY = 'YOUR API KEY'
-CONSUMER_SECRET = 'ENTER YOUR API SECRET'
-
-oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+oauth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
 
 # Initiate the connection to Twitter REST API
 twitter = Twitter(auth=oauth)
 
-###############################################
 # Search API            
 # Search for latest tweets about "#nlproc"
 twitter.search.tweets(q='#nlproc')
+# print json.dumps(twitter.search.tweets(q='#nlproc'), indent=4)
 
 # Search for 10 latest tweets about “#nlproc” in English
 twitter.search.tweets(q='#nlproc', result_type='recent', lang='en', count=10)
+# print json.dumps(twitter.search.tweets(q='#nlproc', result_type='recent', lang='en', count=10), indent=4)
 
 ###############################################
 # Trends API
 # Request worldwide trends
 world_trends = twitter.trends.available(_woeid=1)
+print json.dumps(world_trends, indent=4)
 
 # Get all (it's always 10) trending topics in San Francisco (its WOEID is 2487956)
 sfo_trends = twitter.trends.place(_id = 2487956)
